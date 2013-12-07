46f15a42846f94e11873b1d7e4c56c2dbf77d27b
Adam Getchell <acgetchell@ucdavis.edu>
2013-12-07 15:14:13 -0800

    Nice format output
    There is still a problem due to transposition

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 # http://learnpythonthehardway.org/book/ex15.html
 
+from __future__ import division
+# This allows integer division to convert to floating point division automatically
 from sys import argv
 
 script, filename = argv
@@ -18,8 +20,10 @@ print "Here's your line-by-line counts: "
 transposed = zip(*lines)
 line_number = 1
 for i in transposed:
-	print "In line number %i" % line_number
-	counts = (i.count('G'), i.count('A'))
-	print "There are {0} Gs and {1} As".format(*counts)
-	#print "There are %s Cs" & i.count('C')
+	length = len(tuple(i))
+	A_percent = (i.count('A')/length)*100
+	C_percent = (i.count('C')/length)*100
+	G_percent = (i.count('G')/length)*100
+	T_percent = (i.count('T')/length)*100
+	print "Position: %i:A(%i)%%,C:(%i)%%, G:(%i)%%, T:(%i)%%" % (line_number, A_percent, C_percent, G_percent, T_percent)
 	line_number +=1
