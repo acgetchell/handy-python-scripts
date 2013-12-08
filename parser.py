502fb63ef4f09be6bdf28199801793bc90184ca2
Adam Getchell <acgetchell@ucdavis.edu>
2013-12-07 17:12:03 -0800

    Document better

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-# http://learnpythonthehardway.org/book/ex15.html
+# Parses simple text data files with arrays of characters
 
 from __future__ import division
 # This allows integer division to convert to floating point division automatically
@@ -17,14 +17,17 @@ for i in lines:
 	print i
 
 print "Here's your position-in-line counts: "
+# zip() truncates, so we use Map and fill in uneven array values with None
 transposed = map(None, *lines)
 line_number = 1
 for i in transposed:
+	# We only want to count occurrences of characters
 	length = i.count('A')+i.count('C')+i.count('G')+i.count('T')
+	# The last line will have \n which we want to ignore
 	if length != 0:
 		A_percent = (i.count('A')/length)*100
 		C_percent = (i.count('C')/length)*100
 		G_percent = (i.count('G')/length)*100
 		T_percent = (i.count('T')/length)*100
-		print "Position %i:A(%i)%%,C:(%i)%%, G:(%i)%%, T:(%i)%%" % (line_number, A_percent, C_percent, G_percent, T_percent)
+		print "Position %i:A(%i)%%, C:(%i)%%, G:(%i)%%, T:(%i)%%" % (line_number, A_percent, C_percent, G_percent, T_percent)
 		line_number +=1
